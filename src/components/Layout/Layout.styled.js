42d828a6353eee4253f3b2b26282a549c8822bf6
ease <easeanger@gmail.com>
2022-07-31 18:59:37 +0530

    removed styled components from whole project

--- src/components/Layout/Layout.styled.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-export const LayoutContainer = styled.section`
-  padding: 1rem;
-`;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { LayoutContainer } from "./Layout.styled";
+
 
 const Layout = ({ children }) => {
-  return <LayoutContainer>{children}</LayoutContainer>;
+  return <div className="p-4">{children}</div>;
 };
 
 export default Layout;
