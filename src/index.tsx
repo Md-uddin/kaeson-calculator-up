7f3a5dd7c0548a0153dc52d4ff7aa2bb193981b7
ease <easeanger@gmail.com>
2022-08-03 16:47:52 +0530

    added type decl files from dev to prod dep

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,10 @@ import App from "./App";
 import store from "./store/store";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </>,
   document.getElementById("root")
 );
